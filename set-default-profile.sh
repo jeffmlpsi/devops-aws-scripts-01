eb147ea2901bcdf186fdde1e10d4663109423aff
Jeff Miller <jeffmlpsi@gmail.com>
2025-05-21 18:25:21 -0700

    removed unused

--- set-default-profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "setting default aws profile"
-cp ~/.aws/credentials-default ~/.aws/credentials
-echo "default profile" > ~/.aws/current-profile.txt
-echo "aws profile set to $(cat ~/.aws/current-profile.txt)"
-aws sts get-caller-identity | jq -r '.Arn'
--- set-sso-profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "setting sso aws profile"
-cp ~/.aws/credentials-sso ~/.aws/credentials
-echo "sso profile" > ~/.aws/current-profile.txt
-echo "profile set to $(cat ~/.aws/current-profile.txt)"
-aws sts get-caller-identity | jq -r '.Arn'
